04ef2cc1fd3f7477280433e584317e8dab7d013f
LeonidHr <hristenokleonid@gmail.com>
2022-06-19 15:16:02 +0300

    add index.php and json

--- sendmail.php
+++ sendmail.php
@@ -1,12 +1,4 @@
-<$php 
-  use PHPMailer/PHPMailer/PHPMailer;
-  use PHPMailer/PHPMailer/Exception;
-
-  require 'phpmailer/scr/Exception.php';
-  require 'phpmailer/scr/PHPMailer.php';
-  
-  $mail = new PHPMailer(true);
-  $mail->CharSet = 'UTF-8';
+<$php use PHPMailer/PHPMailer/PHPMailer; use PHPMailer/PHPMailer/Exception; require 'phpmailer/scr/Exception.php' ; require 'phpmailer/scr/PHPMailer.php' ; $mail=new PHPMailer(true); $mail->CharSet = 'UTF-8';
   $mail->setLanguage('ru', 'phpmailer/language/');
   $mail->isHTML(true);
 
@@ -18,35 +10,28 @@
   $body = '<h1>Новый заказ!</h1>';
 
   if(trim(!empty($_POST['name']))) {
-    $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+  $body.='<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
   }
 
   if(trim(!empty($_POST['email']))) {
-    $body.='<p><strong>Email:</strong> '.$_POST['email'].'</p>';
+  $body.='<p><strong>Email:</strong> '.$_POST['email'].'</p>';
   }
 
   if(trim(!empty($_POST['message']))) {
-    $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
+  $body.='<p><strong>Сообщение:</strong> '.$_POST['message'].'</p>';
   }
 
 
   $mail->Body = $body;
 
   if(!$mail->send()) {
-    $message = 'Ошибка';
+  $message = 'Ошибка';
   } else {
-    'Данные отправлены';
+  'Данные отправлены';
   }
 
   $response = ['message' => $message];
 
   header('Content-type: application/json');
   echo json_encode($response);
-?>
-
-
-
-
-
-
-
+  ?>
