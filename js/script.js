773a07ce7aa01d62d317b2d065854761e57e594c
LeonidHr <hristenokleonid@gmail.com>
2022-06-22 17:17:25 +0300

    new icon

--- js/script.js
+++ js/script.js
@@ -1,158 +1,158 @@
 //* Ой зря ты сюда полез
 
-const iconMenu = document.querySelector('.icon-menu'),
-      logoImgsB = document.querySelector('.logo__images--b'),
-      logoImgsW = document.querySelector('.logo__images--w'),
-      menu = document.querySelector('.menu'),
-      header = document.querySelector('.header'),
-      iconMenuInner = document.getElementById('icon-menu-inner');
+"use strict";
+document.addEventListener("DOMContentLoaded", function () {
+
+  const iconMenu = document.querySelector('.icon-menu'),
+        logoImgsB = document.querySelector('.logo__images--b'),
+        logoImgsW = document.querySelector('.logo__images--w'),
+        menu = document.querySelector('.menu'),
+        header = document.querySelector('.header'),
+        iconMenuInner = document.getElementById('icon-menu-inner');
 
 
-function showMenu() {
-  iconMenu.classList.toggle('_remove');
-  iconMenuInner.classList.toggle('_active');
-  menu.classList.toggle('_active');
-  document.body.classList.toggle('_lock');
-}
-
-iconMenu.addEventListener("click", () => {
-  showMenu();
-  iconMenuInner.addEventListener("click", showMenu);
-});
-
-
-//* функция для показа хедера при скролле
-
-var lastScrollTop = 0;
-$(window).scroll(function(event){
-  var st = $(this).scrollTop();
-  if (st > lastScrollTop){
-    header.classList.add('_scroll');
-  } else {
-    header.classList.remove('_scroll');
+  function showMenu() {
+    iconMenu.classList.toggle('_remove');
+    iconMenuInner.classList.toggle('_active');
+    menu.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
   }
-  lastScrollTop = st;
-});
 
-//* навигация по сайту
-
-$(document).ready(function(){
-  $("#navigation").on("click",".menu__link, .btn-offer", function (event) {
-    //отменяем стандартную обработку нажатия по ссылке
-    event.preventDefault();
-    //забираем идентификатор бока с атрибута href
-    let id  = $(this).attr('href'),
-    //узнаем высоту от начала страницы до блока на который ссылается якорь
-    top = $(id).offset().top;
-    //анимируем переход на расстояние - top за 1500 мс
-    $('body,html').animate({scrollTop: top}, 1000);
+  iconMenu.addEventListener("click", () => {
+    showMenu();
+    iconMenuInner.addEventListener("click", showMenu);
   });
-});
+
+
+  //* функция для показа хедера при скролле
+
+  var lastScrollTop = 0;
+  $(window).scroll(function(event){
+    var st = $(this).scrollTop();
+    if (st > lastScrollTop){
+      header.classList.add('_scroll');
+    } else {
+      header.classList.remove('_scroll');
+    }
+    lastScrollTop = st;
+  });
+
+  //* навигация по сайту
+
+  $(document).ready(function(){
+    $("#navigation").on("click",".menu__link, .btn-offer", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+      //забираем идентификатор бока с атрибута href
+      let id  = $(this).attr('href'),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 1000);
+    });
+  });
 
 
 
-//* swiper
+  //* swiper
 
-var swiper = new Swiper(".swiper", {  
-  direction: getDirection(),
-  
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  var swiper = new Swiper(".swiper", {  
+    direction: getDirection(),
+    
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 
-  loop: true,
+    loop: true,
 
-  autoplay: {
-    stopOnLastSlide: false,
+    autoplay: {
+      stopOnLastSlide: false,
 
-  },
+    },
 
-});
+  });
 
-function getDirection() {
-  var windowWidth = window.innerWidth;
-  var direction = window.innerWidth <= 992 ? 'horizontal' : 'vertical';
+  function getDirection() {
+    var windowWidth = window.innerWidth;
+    var direction = window.innerWidth <= 992 ? 'horizontal' : 'vertical';
 
-  return direction;
-}
+    return direction;
+  }
 
 
 
 //* адаптивная высота для слайдера
-// слайдеры 
-const sliderSwiperAbout = document.querySelector('.swiper-about'),
-      sliderSwiperPortfolio = document.querySelector('.swiper-portfolio');
+  // слайдеры 
+  const sliderSwiperAbout = document.querySelector('.swiper-about'),
+        sliderSwiperPortfolio = document.querySelector('.swiper-portfolio');
 
-// сам контент
-const swiperContentPortfolio = document.querySelectorAll('.content-swiper-portfolio'),
-      swiperContentAbout = document.querySelectorAll('.content-swiper-about');
+  // сам контент
+  const swiperContentPortfolio = document.querySelectorAll('.content-swiper-portfolio'),
+        swiperContentAbout = document.querySelectorAll('.content-swiper-about');
 
-// итоговая высота
-let heightContAbout = 0,
-    heightContPortfolio = 0;
+  // итоговая высота
+  let heightContAbout = 0,
+      heightContPortfolio = 0;
 
-// задаем высоту свайперам
-const getSwiperHeight = () => {
-  meterAboutHeight();
-  meterPortfolioHeight();
+  // задаем высоту свайперам
+  const getSwiperHeight = () => {
+    meterAboutHeight();
+    meterPortfolioHeight();
 
-  // добавляем отступ
-  heightContAbout += 150;
-  heightContPortfolio /= 1.9;
+    // добавляем отступ
+    heightContAbout += 150;
+    heightContPortfolio /= 1.9;
 
-  if (window.innerWidth <= 767) {
-    heightContAbout -= 150;
-  }
-
-  //задаем стили
-  sliderSwiperAbout.style.height = `${heightContAbout}px`;
-  sliderSwiperPortfolio.style.height = `${heightContPortfolio}px`;
-
-}
-
-// считаем выcоту about
-const meterAboutHeight = () => {
-  for (let j = 0; j < swiperContentAbout.length; j++) {
-    for (let i = 0; i < swiperContentAbout[j].children.length; i++) {
-      heightContAbout = 
-        swiperContentAbout[j].children[i].clientHeight + swiperContentAbout[j].children[i].clientHeight;
-    }
-  }
-}
-
-// считаем выcоту portfolio
-const meterPortfolioHeight = () => {
-  for (let j = 0; j < swiperContentPortfolio.length; j++) {
-    for (let i = 0; i < swiperContentPortfolio[j].children.length; i++) {
-      heightContPortfolio = 
-        swiperContentPortfolio[j].children[i].clientHeight + swiperContentPortfolio[j].children[i].clientHeight;
-    }
-  }
-}
-
-getSwiperHeight();
-
-//* offer cards click
-
-"use strict"
-
-const offerKards = document.querySelectorAll('.offer__kard');
-const kardsContainer = document.getElementById('kards-container');
-
-for (let i = 0; i < offerKards.length; i++) {
-  offerKards[i].addEventListener("click", function (event) {
-    for (let j = 0; j < offerKards.length; j++) {
-      offerKards[j].classList.remove('_active');
+    if (window.innerWidth <= 767) {
+      heightContAbout -= 150;
     }
 
-    event.currentTarget.classList.add('_active');
-  });
-}
+    //задаем стили
+    sliderSwiperAbout.style.height = `${heightContAbout}px`;
+    sliderSwiperPortfolio.style.height = `${heightContPortfolio}px`;
+
+  };
+
+  // считаем выcоту about
+  const meterAboutHeight = () => {
+    for (let j = 0; j < swiperContentAbout.length; j++) {
+      for (let i = 0; i < swiperContentAbout[j].children.length; i++) {
+        heightContAbout = 
+          swiperContentAbout[j].children[i].clientHeight + swiperContentAbout[j].children[i].clientHeight;
+      }
+    }
+  };
+
+  // считаем выcоту portfolio
+  const meterPortfolioHeight = () => {
+    for (let j = 0; j < swiperContentPortfolio.length; j++) {
+      for (let i = 0; i < swiperContentPortfolio[j].children.length; i++) {
+        heightContPortfolio = 
+          swiperContentPortfolio[j].children[i].clientHeight + swiperContentPortfolio[j].children[i].clientHeight;
+      }
+    }
+  };
+
+  getSwiperHeight();
+
+  //* offer cards click
+
+  const offerKards = document.querySelectorAll('.offer__kard');
+  const kardsContainer = document.getElementById('kards-container');
+
+  for (let i = 0; i < offerKards.length; i++) {
+    offerKards[i].addEventListener("click", function (event) {
+      for (let j = 0; j < offerKards.length; j++) {
+        offerKards[j].classList.remove('_active');
+      }
+
+      event.currentTarget.classList.add('_active');
+    });
+  }
 
 //* form ==============================================================
 
-document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById('form');
   const formError = document.querySelector('.offer__error');
   const formOk = document.querySelector('.offer__ok');
@@ -235,7 +235,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // удаляем ошибку
   function formRemoveError (input) {
-    input.classList.remove('_error')
+    input.classList.remove('_error');
   }
 
   // проверяем на правильность емейл
@@ -243,46 +243,24 @@ document.addEventListener("DOMContentLoaded", function () {
     return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
   }
 
-});
 
 //* споллер для футера =======================================================
 
-if (window.innerWidth <= 992) {
-  $(document).ready(function () {
-    $('.footer__item-top').click(function (event) {
-      if ($('.footer__item').hasClass('one')) {
-        $('.footer__item-top').not($(this)).removeClass('_active');
-        $('.footer__item-content').not($(this).next()).slideUp(300);
-      }
-  
-      $(this).toggleClass('_active').next().slideToggle(300);
+  if (window.innerWidth <= 992) {
+    $(document).ready(function () {
+      $('.footer__item-top').click(function (event) {
+        if ($('.footer__item').hasClass('one')) {
+          $('.footer__item-top').not($(this)).removeClass('_active');
+          $('.footer__item-content').not($(this).next()).slideUp(300);
+        }
+    
+        $(this).toggleClass('_active').next().slideToggle(300);
+      });
     });
-  });
-  
-}
-
-
-
-
- /*
-  const footerTitle = document.querySelectorAll('.footer__title');
-  const footerText = document.querySelectorAll('.footer__text');
-
-  footerTitle.forEach(title => {
-    title.addEventListener("click", openSpoiler);
-  });
-
-  function openSpoiler (e) {
-    for (let i = 0; i < footerTitle.length; i++) {
-      if (footerTitle[i] != e.currentTarget) {
-        footerText[i].classList.remove('_open');
-      }
-      if (footerTitle[i] == e.currentTarget) {
-        footerText[i].classList.toggle('_open');
-      }
-    }
+    
   }
-  */
+
+});
 
 
 
